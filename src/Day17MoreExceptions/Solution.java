58052027b97aeea3e3cb9484ca89eb867ae6abd7
cesaralducinruiz <cesaralducinruiz@192.168.1.72>
2019-11-24 22:41:23 -0600

    Ejemplos de certificacion Java 8

--- src/Day17MoreExceptions/Solution.java
+++ src/Day17MoreExceptions/Solution.java
@@ -5,36 +5,38 @@
  */
 package Day17MoreExceptions;
 
-
 import java.util.*;
 import java.io.*;
 
-
 class Calculator {
 
-    int power(int n, int p) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    int power(int n, int p) throws Exception {
+
+        if (n < 0 && p < 0) {
+            throw new Exception("n and p should be non-negative");
+
+        }
+
+        return (int) Math.pow(n, p);
     }
-    
+
 }
 
-
-class Solution{
+class Solution {
 
     public static void main(String[] args) {
-    
+
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         while (t-- > 0) {
-        
+
             int n = in.nextInt();
             int p = in.nextInt();
             Calculator myCalculator = new Calculator();
             try {
                 int ans = myCalculator.power(n, p);
                 System.out.println(ans);
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
